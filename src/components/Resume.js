6bbf5f2e2efc46396a37ddbce443345759c6f960
Shivam Kumar Singh <57297788+Shivamsssss@users.noreply.github.com>
2020-11-13 23:43:46 +0530

    updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,138 @@
 import React from 'react';
+import react from '../assets/icons/react.svg'
+import python from '../assets/icons/python.svg'
+import Bar from './Bar';
+
+
+const languages = [
+    {
+        icon: python,
+        name: 'C',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'C++',
+        level: '60'
+    },
+    {
+        icon: react,
+        name: 'HTML',
+        level: '75'
+    },
+    {
+        icon: react,
+        name: 'CSS',
+        level: '60'
+    },
+    {
+        icon: react,
+        name: 'React',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'javascript',
+        level: '65'
+    },
+    
+    {
+        icon: react,
+        name: 'Python',
+        level: '70'
+    }
+
+]
+
+const tools = [
+
+    {
+        icon: react,
+        name: 'GCP',
+        level: '70'
+    },
+    {
+        icon: react,
+        name: 'OBS Studio',
+        level: '45'
+    },
+    {
+        icon: react,
+        name: 'Git',
+        level: '60'
+    },
+    {
+        icon: react,
+        name: 'Matlab',
+        level: '45'
+    },
+
+]
+
 
 const Resume = () => {
     return (
-        <div>
-            <h1>This is resume</h1>
+        <div className="container resume">
+            <div className="row">
+                <div className="col-lg-6 resume-card" >
+                    <h4 className="resume-card_heading">
+                        Education
+                    </h4>
+                    <div className="resume-card_body">
+                        <h5 className="resume-card_title">
+                            Computer Science and Enginnering
+                        </h5>
+                        <p className="resume-card_name">
+                           <i> Indian Institute of Information and Technology, Kota, Rajasthan (Mnit-Campus).</i>
+                        </p>
+                        <p className="resume-card_details">
+                           <strong><i>Hello</i>, I am 2nd year student (Sophomore), persuing B.tech in Computer Science Engineering from <i>IIITkota</i></strong>
+                        </p>
+                    </div>
+                </div>
+                <div className="col-lg-6 resume-card" >
+                    <h4 className="resume-card_heading">
+                        Experience
+                    </h4>
+                    <div className="resume-card_body">
+                        <h5 className="resume-card_title">
+                            Intern as a Campus Ambassador
+                        </h5>
+                        <p className="resume-card_name">
+                            <i>At International Model United Nation Online-mode (jun-july)</i>
+                            
+                        </p>
+                        <p className="resume-card_details">
+                            <strong>I work as a campus Ambassador for 1-2 months in IMUN for learning new concepts and  to interact with diversity</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-lg-6 resume-languages">
+                    <h5 className="reume-language_heading">
+                        Language and Framework
+                    </h5>
+                    <div className="resume-language_body mt-3">
+                        {
+                            languages.map(language =>
+                                <Bar value={language} />
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="col-lg-6 resume-languages">
+                    <h5 className="reume-language_heading">
+                        Tools and Softwares
+                    </h5>
+                    <div className="resume-language_body mt-3">
+                        {
+                            tools.map(tool => <Bar value={tool} />)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,10 +2,13 @@ import React from 'react';
 import facebook from '../assets/icons/facebook.svg';
 import github from '../assets/icons/github.svg';
 import instagram from '../assets/icons/instagram.svg';
-import tie from '../assets/icons/tie.svg';
-import pin from '../assets/icons/pin.svg';
+import download from '../assets/icons/download.svg';
+import location from '../assets/icons/location.svg';
 import image from '../assets/image.jpg';
-import Resume from '../assets/Resume.pdf'
+import Resume from '../assets/Resume.pdf';
+import Linkedin from '../assets/icons/Linkedin.svg';
+import twitter from '../assets/icons/twitter.svg';
+import mail from '../assets/icons/mail.svg';
 
 function Sidebar() {
     const mailhandle = () => {
@@ -18,24 +21,28 @@ function Sidebar() {
             <div className="sidebar_item sidebar_title">Web Devloper</div>
             <a href={Resume} download="Resume.pdf">
                 <div className="sidebar_item sidebar_resume">
-                    <img src={tie} alt="resume" className="sidebar_icon" />Download Resume
+                    <img src={download} alt="resume" className="sidebar_icon mr-1" />Download Resume
                 </div>
             </a>
-            <figure className="sidebar_social-icons my-2">
+            <figure className="sidebar_social-icons my-3">
                 <a href="!#"> <img src={facebook} alt="facebook" className="sidebar_icon mr-3" /></a>
-                <a href="!#"> <img src={instagram} alt="instagram" className="sidebar_icon" /></a>
+                <a href="!#"> <img src={instagram} alt="instagram" className="sidebar_icon mr-3" /></a>
+                <a href="!#"> <img src={Linkedin} alt="Linkedin" className="sidebar_icon mr-3" /></a>
+                <a href="!#"> <img src={twitter} alt="twitter" className="sidebar_icon" /></a>
             </figure>
             <div className="sidebar_contact">
                 <div className="sidebar_item sidebar_github">
-                    <a href="!#"><img src={github} alt="github" className="sidebar_icon mr-3" />GitHub</a>
+                    <a href="!#"><img src={github} alt="github" className="sidebar_icon mr-1" />GitHub</a>
                 </div>
                 <div className="sidebar_location">
-                    <img src={pin} alt="location" className="sidebar_icon mr-1" />Agra, India
+                    <img src={location} alt="location" className="sidebar_icon mr-1" />Agra, India
                 </div>
-                <div className="sidebar_item">shivamscool11@gmail.com</div>
-                <div className="sidebar_item">9368060897,9456463734 </div>
+                
+                <div className="sidebar_item">9368060897, 9456463734 </div>
             </div>
+            
             <div className="sidebar_item sidebar_email" onClick={mailhandle}>Email me</div>
+            
         </div>
 
 
